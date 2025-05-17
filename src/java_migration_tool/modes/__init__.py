4d24c7857b0266503f78453a1cf915c6e0546dd7
Mohammad Mazraeh <mazraeh.mohammad@gmail.com>
2025-05-17 16:58:46 -0700

    sample agentic implementation that shows similar migration plan

---
+++ src/java_migration_tool/modes/__init__.py
@@ -0,0 +1,4 @@
+from java_migration_tool.modes.agentic import run_agentic_migration
+from java_migration_tool.modes.sequential import run_sequential_migration
+
+__all__ = ["run_agentic_migration", "run_sequential_migration"]
